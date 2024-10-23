0d5a24c7e54aa07fe5cb36db3ab36a65dfb25ca7
Deep2804 <deepak28042002singh@gmail.com>
2024-10-23 22:33:30 +0530

    Operator and Conditionals

---
+++ Javascript/Lecture 41/index.js
@@ -0,0 +1,148 @@
+// Arithmetic Operator
+// console.log(2+3);
+// console.log(5-3);
+// console.log(2*3);
+// console.log(6/3);
+// console.log(10%3);
+
+// Assignment Operator
+
+// let a = 3
+// let b = 2
+
+// a += b;
+// console.log(a);
+
+// a -= b;
+// console.log(a);
+
+// a *= b
+// console.log(a);
+
+// a /= b
+// console.log(a);
+
+// a %= b
+// console.log(a);
+
+
+// Comparison Operator
+// console.log(10 > 5);
+// console.log(10 < 5);
+// console.log(10 >= 5);
+// console.log(10 <= 5);
+
+// console.log(5 == 5)
+// console.log(5 === 5)
+
+// console.log('5' == 5)
+// console.log('5' === 5)
+
+// console.log( 10 != 5)
+// console.log( 10 !== 10)
+
+// Ternary Operator
+// let age = 22
+// let status1 = (age > 18) ? 'I can Vote' : 'I cannot Vote';
+// console.log(status1);
+
+// Logical Operator
+// let ans = (true && true && true)
+// console.log(ans);
+
+// let result1 = (true || false || false)
+// console.log(result1);
+
+// let result2 = !(true)
+// console.log(result2);
+
+// Non-Boolean Operator
+// console.log(false || 7)
+
+// Short circuting 
+// console.log(false || 7 || 18 || 23);
+
+// Bitwise Operator
+// console.log(2 & 5)
+// console.log(2 | 5)
+// console.log(~(0));
+// console.log(2 ^ 2);
+// console.log(5 << 1)
+// console.log(10 >> 1)
+
+
+
+// Conditionals Statement
+
+// 1. If statement
+// let age = 22
+// if(age >= 18){
+//     console.log('Can Vote');
+// }
+
+
+// 2. If-else statement
+// let age = 22;
+// if(age >= 18){
+//     console.log('Can Vote');
+// }
+// else {
+//     console.log('Cannot Vote');
+// }
+
+// 3. If-else-If statement
+// let number = 5;
+// if(number == 1){
+//     console.log('A');
+// } 
+// else if(number == 2){
+//     console.log('B');
+// }
+// else if(number == 3){
+//     console.log('C');
+// }
+// else if(number == 4){
+//     console.log('D');
+// }
+// else {
+//     console.log('E');
+// }
+
+// Nested If-else 
+// let number = 4;
+// let age = 22;
+// if(number == 1){
+//     console.log('A');
+// } 
+// else if(number == 2){
+//     console.log('B');
+// }
+// else if(number == 3){
+//     console.log('C');
+// }
+// else if(number == 4){
+//     if(age>=18){
+//         console.log('can vote');
+//     }
+//     console.log('D');
+// }
+// else {
+//     console.log('E');
+// }
+
+
+// Switch Statement
+let num = 3;
+switch(num){
+    case 1 : console.log('A');
+    break;
+    case 2 : console.log('B');
+    break;
+    case 3 : console.log('C');
+    break;
+    case 4 : console.log('D');
+    break;
+    case 5 : console.log('E');
+    break;
+    default: console.log('F');
+}
