b279a3ed2783157b4b50c5bb78b962e4326006c8
Deep2804 <deepak28042002singh@gmail.com>
2024-10-23 22:33:01 +0530

    Variable and Datatypes

---
+++ Javascript/Lecture 40/index.js
@@ -0,0 +1,48 @@
+// var age = 30;
+
+// if(true){
+//     console.log(age);
+// }
+
+// function solve(){
+//     var age = 25
+//     console.log(age)
+// }
+
+// solve()
+// console.log(age)
+
+// let a  = 10;
+// console.log(a);
+
+// let a = 10;
+// let a = 20;
+
+// let a = 10
+// a = "Deepak"
+// a = true
+// a = null
+
+
+// const a = 29
+// console.log(a);
+
+let marks = 20
+marks = "Deepak";
+marks = true
+console.log(marks);
+
+let mark;
+console.log(mark)
+
+let marks1 = null
+console.log(marks1)
+
+
+let number1 = 9393993939399595858935837573953834893438947392837239728642482642984
+console.log(number1)
+
+
+
+
+
