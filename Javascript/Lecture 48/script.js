d13963c3b82555e8ee23aae875cf26fe485f464e
Deep2804 <deepak28042002singh@gmail.com>
2024-11-11 22:16:39 +0530

    Common In-built Object

---
+++ Javascript/Lecture 48/script.js
@@ -0,0 +1,37 @@
+console.log("Deepak");
+
+// Math Object
+console.log(Math.PI);
+
+console.log(Math.max(60,30,24,600,712,89));
+
+console.log(Math.min(60,30,24,600,712,89));
+
+console.log(Math.round(1.6));
+
+console.log(Math.floor(1.9));
+
+console.log(Math.ceil(1.1));
+
+console.log(Math.abs(-5));
+
+console.log(Math.random());
+
+console.log(Math.sqrt(9));
+
+console.log(Math.pow(2,10));
+
+
+// Date object
+let curr = new Date();
+
+let date = new Date('June 20 1998 07:15');
+
+let newDate = new Date(1998,5,20,7);
+
+console.log(newDate);
+console.log(newDate.getDay());
+console.log(newDate.getFullYear());
+console.log(newDate.setFullYear(2001));
+
+console.log(newDate);
